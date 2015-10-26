8973fa63ac08f7e4da21e3841fce3a5a1b79f609
Darkemon <a.krasotin@yahoo.com>
2015-10-26 12:53:27 +0300

    add AtExit handler

--- node-syslog.h
+++ node-syslog.h
@@ -14,26 +14,24 @@ using v8::Value;
 
 class Syslog : public node::ObjectWrap {
   public:
-    static void Initialize(Local<Object> exports);
-	    
-  protected:
     static Persistent<FunctionTemplate> constructor_tpl;
 
+    Syslog();
+    ~Syslog();
+
+    static void Open(int option, int facility);
+    static void Close();
+
+    static void Initialize(Local<Object> exports);
     static void Init(const FunctionCallbackInfo<Value>& args);
     static void Log(const FunctionCallbackInfo<Value>& args);
     static void LogSync(const FunctionCallbackInfo<Value>& args);
     static void SetMask(const FunctionCallbackInfo<Value>& args);
     static void Destroy(const FunctionCallbackInfo<Value>& args);
-	
-    Syslog();
-    ~Syslog();
 
   private:
     static bool connected_;
     static char name_[1024];
-
-    static void Open(int option, int facility);
-    static void Close();
 };
 
 #endif /* NODE_SYSLOG_H */
